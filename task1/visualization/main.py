b86cc245a185201f607322ec0662a0e4749816f1
marado <78569959+Marado-Programmer@users.noreply.github.com>
2023-12-30 16:58:03 +0000

    drawing plots

--- task1/visualization/main.py
+++ task1/visualization/main.py
@@ -1,21 +1,24 @@
 from pathlib import Path
 from sys import stderr
 from time import time
+from typing import BinaryIO
 
+import matplotlib.pyplot as plt
 from pandas import DataFrame
 from serial import Serial
 from serial.tools.list_ports import comports
+from serial.tools.list_ports_common import ListPortInfo
 
 MIN_ROWS = 10
 
 
-def create_serial(port_info):
+def create_serial(port_info: ListPortInfo) -> Serial:
     return Serial(port=port_info.device,
                   baudrate=115200,
                   timeout=1)
 
 
-def get_n_rows():
+def get_n_rows() -> int:
     n = int(input('How many rows of data you want to receive:\t'))
 
     if n < MIN_ROWS:
@@ -23,6 +26,44 @@ def get_n_rows():
               file=stderr)
 
     return max(MIN_ROWS, n)
+
+
+def write_csv(file_stream: BinaryIO, ser: Serial, n_rows: int) -> (list[str], dict[str, list[float]]):
+    first = True
+
+    h = []
+    d = {}
+    while n_rows >= 0:  # first line will be the header so does not count as a row
+        line = str(ser.readline())
+        split = line.strip().split(separator)
+
+        if first:
+            h = split
+            for column in h:
+                d[column] = []
+            file_stream.write(bytes(line.strip(), "utf-8"))
+            first = False
+        else:
+            for index, v in enumerate(split):
+                d[h[index]].append(float(v))
+
+            file_stream.write(bytes(f"{delimiter}{line.strip()}", "utf-8"))
+
+        n_rows -= 1
+
+    return h, d
+
+
+def draw_plots(columns: list[str], data_dict: dict[str, list[float]] | DataFrame) -> None:
+    x_column = columns[0]
+    for c in columns[1:]:
+        plt.title(f"Graph {x_column}-{c}")
+        plt.xlabel(x_column)
+        plt.ylabel(c)
+
+        plt.plot(data_dict[x_column], data_dict[c])
+
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -33,8 +74,6 @@ if __name__ == '__main__':
             if not serial.is_open:
                 serial.open()
 
-            n_rows = get_n_rows()
-
             directory = "data"
             file_name = f"data_sensor_raw_{serial.name}_{time()}.csv"
 
@@ -42,28 +81,9 @@ if __name__ == '__main__':
             delimiter = "\r\n"
 
             with open(f"{directory}/{file_name}", 'ab') as stream:
-                i = 1
-                first = True
-                header = []
-                data = {}
-                while n_rows >= 0:  # first line will be the header so does not count as a row
-                    line = str(serial.readline())
-                    split = line.strip().split(separator)
+                header, data = write_csv(stream, serial, get_n_rows())
 
-                    if first:
-                        header = split
-                        for column in header:
-                            data[column] = []
-                        stream.write(bytes(line.strip(), "utf-8"))
-                        first = False
-                    else:
-                        for index, v in enumerate(split):
-                            data[header[index]].append(v)
+            data_frame = DataFrame(data)
+            data_frame.to_csv(f"{directory}/pandas_{file_name}", index=False)
 
-                        stream.write(bytes(f"{delimiter}{line.strip()}", "utf-8"))
-
-                    i += 1
-                    n_rows -= 1
-
-                data_frame = DataFrame(data)
-                data_frame.to_csv(f"{directory}/pandas_{file_name}", index=False)
+            draw_plots(header, data_frame)
